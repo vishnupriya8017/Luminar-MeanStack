c46dc5880ab174ed2bc0f4ffb08dd90005e54775
vishnupriya <vishnupriya8017@gmail.com>
2021-03-31 10:21:46 +0530

    javascript file commited

---
+++ jsprograms/first.js
@@ -0,0 +1,4 @@
+var name="Vishnupriya P";
+var age=24;
+//console.log("hai,I'm "+name,+age, "years old."); 
+console.log(`my name is ${name} im ${age} years old`);
---
+++ jsprograms/swapping.js
@@ -0,0 +1,6 @@
+var num1=10;
+var num2=20;
+var num3=num1;
+var num1=num2;
+var num2=num3;
+console.log("num1 is",+num1+" num2 is",+num2);
